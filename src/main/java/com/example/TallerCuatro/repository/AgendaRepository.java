86d7ed789bffb5ed00fa6ba8264ca4d90312c5d2
oskarRuiz08 <160038336+oskarRuiz08@users.noreply.github.com>
2025-10-24 12:55:52 -0500

    Update AgendaRepository.java
    a

--- src/main/java/com/example/TallerCuatro/repository/AgendaRepository.java
+++ src/main/java/com/example/TallerCuatro/repository/AgendaRepository.java
@@ -3,6 +3,6 @@ package com.example.TallerCuatro.repository;
 import com.example.TallerCuatro.entity.AgendasEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AgendaRepository extends JpaRepository<AgendasEntity, String> {
+public interface AgendaRepository {
 
 }
